5ebcb09326246326425d2a7f5b1f174adec05850
Paul Salaets <psalaets@gmail.com>
2023-06-05 01:09:49 -0500

    Don't need to watch the input dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,4 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addWatchTarget('./src/**/*.js');
-
   eleventyConfig.addFilter('yyyymmdd', (dateObject) => {
     return dateObject.toISOString().slice(0, 'YYYY-MM-DD'.length);
   });
